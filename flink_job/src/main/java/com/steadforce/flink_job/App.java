c3f2a61523f14a834755dd311ec086e443985266
Egor Shevdin <egor.shevdin@steadforce.com>
2024-03-08 13:48:39 +0100

    fix bug 6

--- flink_job/src/main/java/com/steadforce/flink_job/App.java
+++ flink_job/src/main/java/com/steadforce/flink_job/App.java
@@ -147,10 +147,10 @@ public static void main(String[] args) throws Exception {
         // complete_table.executeInsert("db.complete_table");
         // Write the DataStream to the tables
         tableEnv.executeSql(
-               "INSERT INTO db.complete_table SELECT * FROM my_complete_table limit");
+               "INSERT INTO db.complete_table SELECT * FROM my_complete_table");
 
         tableEnv.executeSql(
-               "INSERT INTO db.manipulated_table SELECT * FROM my_manipulated_table limit");
+               "INSERT INTO db.manipulated_table SELECT * FROM my_manipulated_table");
 
         
 
