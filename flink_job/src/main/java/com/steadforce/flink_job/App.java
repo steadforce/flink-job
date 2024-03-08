83ee0422ee4063deb47657e4bd11e45f80c3cb76
Egor Shevdin <egor.shevdin@steadforce.com>
2024-03-08 11:38:46 +0100

    add limit to insert

--- flink_job/src/main/java/com/steadforce/flink_job/App.java
+++ flink_job/src/main/java/com/steadforce/flink_job/App.java
@@ -168,15 +168,17 @@ public static void main(String[] args) throws Exception {
                        + "data STRING"
                        + ")");
         // Write Table to Iceberg
-        manipulated_table.executeInsert("db.manipulated_table");
-        // Write Table to Iceberg
-        complete_table.executeInsert("db.complete_table");
-        // Write the DataStream to the tables
-        // tableEnv.executeSql(
-        //        "INSERT INTO db.complete_table SELECT * FROM my_complete_table");
 
-        // tableEnv.executeSql(
-        //        "INSERT INTO db.manipulated_table SELECT * FROM my_manipulated_table");
+       
+        // manipulated_table.executeInsert("db.manipulated_table");
+        // // Write Table to Iceberg
+        // complete_table.executeInsert("db.complete_table");
+        // Write the DataStream to the tables
+        tableEnv.executeSql(
+               "INSERT INTO db.complete_table SELECT * FROM my_complete_table limit 10");
+
+        tableEnv.executeSql(
+               "INSERT INTO db.manipulated_table SELECT * FROM my_manipulated_table limit 10");
         // Execute the job
         env.execute("Flink Job");
    }
